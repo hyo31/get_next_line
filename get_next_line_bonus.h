b8d1f31c39d9fdf7c37cc63928fecaebf3452941
Milan andrei daniel Groen <mgroen@f0r3s2.codam.nl>
2021-12-06 12:49:32 +0100

    mandatory and bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line_bonus.h                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mgroen <mgroen@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/12 11:59:31 by mgroen        #+#    #+#                 */
+/*   Updated: 2021/10/19 13:14:38 by mgroen        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *str);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strdup(const char *s1);
+int			ft_stop_newline(const char *str);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+
+# endif
+#endif
